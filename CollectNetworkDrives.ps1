df80d880abb70ea4a9533d330e88acd0b58fdcc1
subjectdatadotcom <raja.bandla@subjectdata.com>
2024-10-29 15:24:10 -0500

    Update CollectNetworkDrives.ps1

--- CollectNetworkDrives.ps1
+++ CollectNetworkDrives.ps1
@@ -6,6 +6,5 @@ Get-ChildItem -Path Registry::HKEY_CURRENT_USER\Network | ForEach-Object {
     $driveLetter = $_.PSChildName
     $remotePath = (Get-ItemProperty -Path $_.PSPath).RemotePath
 
-    # Corrected line to avoid command not found error
     "$driveLetter -> $remotePath" | Out-File -FilePath "$directoryPath\$fileName" -Append
 }
